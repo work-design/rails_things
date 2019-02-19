0ac28b0b2557599c4ffa58c62c05c62bdceb005f
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-19 19:33:57 +0800

    set table connection

--- app/models/rails_things/z_thing.rb
+++ app/models/rails_things/z_thing.rb
@@ -1,4 +1,5 @@
 class ZThing < ThingsRecord
   self.table_name = 'ZTHING'
   self.primary_key = 'Z_PK'
+
 end
--- app/models/things_record.rb
+++ app/models/things_record.rb
@@ -1,3 +1,8 @@
 class ThingsRecord < ApplicationRecord
+  self.abstract_class = true
+  establish_connection(
+    adapter: 'sqlite3',
+    database: RailsThings.config.db_path
+  )
 
 end
